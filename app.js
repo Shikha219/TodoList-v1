246491c4a78e9bd41f6e9f8c57f3018a5c79a5e4
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2021-07-21 20:42:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
+
+
+const app = express();
+
+let items = ["Buy Food","Cook Food","Eat Food"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+  let day = date();
+  res.render("list",{listTitle: day,newListItem:items});
+
+});
+
+
+app.post("/",(req,res)=>{
+  let item = req.body.newItem;
+   if(req.body.list ==="Work"){
+     workItems.push(item);
+     res.redirect("/work");
+   }else{
+     items.push(item);
+     res.redirect("/");
+   }
+
+});
+
+app.get("/work",(req,res)=>{
+  res.render("list",{listTitle: "Work List", newListItem:workItems});
+});
+
+app.post("/work",(req,res)=>{
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000,()=>{
+  console.log("Server on port 3000");
+});
